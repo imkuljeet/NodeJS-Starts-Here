e9a4d2d25d033bfc8a7a799cf78dbefb7db2a3bb
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2023-10-03 20:07:17 +0530

    Sending Responses

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    console.log("Kuljeet");
-})
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    const headers = req.headers;
+
+    console.log(url, method, headers);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My created Page</title></head>');
+    res.write('<body>');
+
+    if (url === '/home') {
+        res.write('<h1>Welcome home</h1>');
+    } else if (url === '/about') {
+        res.write('<h1>Welcome to About Us page</h1>');
+    } else if (url === '/node') {
+        res.write('<h1>Welcome to my Node.js project</h1>');
+    } else {
+        res.write('<h1>Welcome/h1>');
+    }
+
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+});
+
 server.listen(4000);
