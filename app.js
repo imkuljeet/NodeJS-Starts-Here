a562d9d11a9fb02476035a02721727aae78b2fa8
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2023-10-04 21:31:45 +0530

    Writing and reading from file

--- app.js
+++ app.js
@@ -1,30 +1,51 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
-    const headers = req.headers;
 
-    console.log(url, method, headers);
+    if (url === '/') {
+        fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
 
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My created Page</title></head>');
-    res.write('<body>');
+            console.log("data from file: " + data);
 
-    if (url === '/home') {
-        res.write('<h1>Welcome home</h1>');
-    } else if (url === '/about') {
-        res.write('<h1>Welcome to About Us page</h1>');
-    } else if (url === '/node') {
-        res.write('<h1>Welcome to my Node.js project</h1>');
+            res.write('<!DOCTYPE html>');
+            res.write('<html><head><title>Enter Message</title></head>');
+            res.write(`<body>${data}</body>`);
+            res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form>');
+            res.write('</html>');
+            return res.end();
+        });
+    } else if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
     } else {
-        res.write('<h1>Welcome/h1>');
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My first Page</title></head>');
+        res.write('<body><h1>Hello from Node.js server</h1></body>');
+        res.write('</html>');
+        res.end();
     }
-
-    res.write('</body>');
-    res.write('</html>');
-    res.end();
 });
 
 server.listen(4000);
