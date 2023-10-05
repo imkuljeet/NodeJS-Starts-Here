c54615cac269e74bd69549ae611b16ca722f430d
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2023-10-06 04:21:22 +0530

    Intro to Middlewares

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 // const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next(); //Allows the request to continue to the next middleware in line.
+// app.use('/',(req,res,next)=>{
+//     console.log('This always runs');
+//     next();
+// });
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/add-product',(req,res,next)=>{
+    // console.log('In another middleware');
+    res.send('<form action ="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req,res,next)=>{
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+
+
+app.use('/',(req,res,next)=>{
     console.log('In another middleware');
     res.send('<h1>Hello From Express</h1>');
 });
