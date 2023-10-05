66ea6a40db7f364df04931076409fedcf2aeb096
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2023-10-06 04:31:17 +0530

    added input tag and size - INTRO TO MIDDLEWARES

--- app.js
+++ app.js
@@ -12,15 +12,36 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/add-product',(req,res,next)=>{
-    // console.log('In another middleware');
-    res.send('<form action ="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+// app.get('/add-product',(req,res,next)=>{
+//     // console.log('In another middleware');
+//     res.send('<form action ="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+// });
+
+app.get('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST">' +
+        '<input type="text" name="title" placeholder="Product Title">' +
+        '<input type="text" name="size" placeholder="Product Size">' +
+        '<button type="submit">Add Product</button>' +
+        '</form>');
 });
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
+
+
+// app.post('/product',(req,res,next)=>{
+//     console.log(req.body);
+//     res.redirect('/');
+// })
+
+app.post('/product', (req, res, next) => {
+    const title = req.body.title;
+    const size = req.body.size;
+    
+    console.log('Title:', title);
+    console.log('Size:', size);
+    
     res.redirect('/');
-})
+});
+
 
 
 
